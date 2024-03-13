dde3d06db3a7e3373506971c15f63d956e52e784
chabieralaiza <calaiza64@gmail.com>
2024-03-13 15:13:51 +0100

    add lazy loading routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./modules/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'heroes',
+    loadComponent: () =>
+      import('./modules/heroes/pages/heroes-list/heroes-list.component').then(
+        (m) => m.HeroesListComponent
+      ),
+  },
+  {
+    path: 'heroes/:id',
+    loadComponent: () =>
+      import(
+        './modules/heroes/pages/heroes-detail/heroes-detail.component'
+      ).then((m) => m.HeroesDetailComponent),
+  },
+  { path: '**', redirectTo: '/home' },
+];
